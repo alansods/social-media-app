7c34e935ecf44f1ced7c1f9bcf87923b6f1c826a
Alan Santos <alansods@gmail.com>
2022-01-14 16:14:26 -0300

    funcao criar post

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,46 +1,77 @@
 <template>
   <div id="home">
-
     <div class="newpost">
       <h2>Bem vindo de volta!</h2>
       <span>Compartilhe seu dia</span>
-      <textarea placeholder="O que está fazendo hoje?" rows="15">
-
+      <textarea
+        placeholder="O que está fazendo hoje?"
+        rows="15"
+        v-model="input"
+      >
       </textarea>
-      <button>Compartilhar</button>
+      <button @click="createPost">Compartilhar</button>
     </div>
 
     <div class="postarea">
       <article class="post">
         <h1>Alan</h1>
-        <p>
-          Olá, este é meu primeiro post aqui.
-        </p>
+        <p>Olá, este é meu primeiro post aqui.</p>
         <div class="action-post">
           <button>20 curtidas</button>
           <button>Veja post completo</button>
         </div>
       </article>
     </div>
-
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Home',
+import firebase from '@/services/firebaseConnection'
 
-}
+export default {
+  name: "Home",
+  data() {
+    return {
+      input: "",
+      user: {},
+    };
+  },
+  create() {
+    const user = localStorage.getItem("SocialApp");
+    this.user = JSON.parse(user);
+  },
+  methods: {
+    async createPost() {
+      if (this.input === "") {
+        return;
+      }
+
+      await firebase.firestore().collection("posts").add({
+        created: new Data(),
+        content: this.input,
+        autor: this.user.nome,
+        userId: this.user.uid,
+        likes: 0,
+      })
+      .then(()=> {
+        this.input = '';
+        console.log('Post criado com sucesso.')
+      })
+      .catch((error)=> {
+        console.log('Post criado com sucesso.' + error)
+      })
+
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 #home {
   display: flex;
   flex-direction: row;
   margin: 25px;
 }
 
-@import './home.css';
-
+@import "./home.css";
 </style>
